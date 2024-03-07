653b254e193f98a63998874c73f7bc75e2a6fa6d
Sergi Rovira Morral <sroviramorral@gmail.com>
2024-03-07 10:35:44 +0100

    Setting auto-reload on new day

--- src/App.js
+++ src/App.js
@@ -12,29 +12,68 @@ const App = () => {
   const dispatch = useDispatch();
 
   const [isLogged, setIsLogged] = useState(false)
+  const [lastFocusDate, setLastFocusDate] = useState(null)
+
 
   useEffect(() => {
+    /**
+    * Checks the authentication state and sets the user data if authenticated
+    * @param {Object} user - the user object
+    * @returns {void}
+    */
     auth.onAuthStateChanged((user) => {
       if (!user) {
         setIsLogged(false);
         return
       }
 
+      // Get user from authorized users database, and load their data.
       firestore.collection('allowedUsers').where('email', '==', user.email).get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          if (doc.exists) {
-            const mergedUserData = { ...user, ...doc.data(), 'id': doc.id }
-            dispatch(setUserData(mergedUserData));
-            setIsLogged(true)
-          } else {
-            dispatch(setUserData(null))
-            setIsLogged(false);
-          }
+          const mergedUserData = doc.exists ? { ...user, ...doc.data(), 'id': doc.id } : null;
+          dispatch(setUserData(mergedUserData))
+          setIsLogged(doc.exists)
         });
       })
     });
+
+
+    
+    /**
+    * Handles the focus event and checks for a new day to reload the page if necessary
+    * @returns {void}
+    */
+    const handleFocus = () => {
+      console.log('Page focused. Check for new day');
+      // If lastFocusDate day is not today, reload
+      const currentDate = new Date();
+      if (lastFocusDate && lastFocusDate.getDate() !== currentDate.getDate()) {
+          console.log('New day. Reloading');
+          window.location.reload();
+      }
+    }
+  
+    /**
+    * Handles the blur event and sets the last focus date
+    * @returns {void}
+    */
+    const handleBlur = () => {
+      console.log('Page blurred. Setting last focus date');
+      setLastFocusDate(new Date());
+      
+    }
+
+    window.addEventListener('focus', handleFocus);
+    window.addEventListener('blur', handleBlur);
+
+    return () => {
+      window.removeEventListener('focus', handleFocus);
+      window.removeEventListener('blur', handleBlur);
+    }
+
     // eslint-disable-next-line
   }, []);
+
 
 
   return <div>{isLogged ? <OrviTasks /> : <Login />}</div>;
